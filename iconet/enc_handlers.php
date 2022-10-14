6ef6f328edbaa64a8b9edc003518a5802ee04043
jana <jana@iconet-foundation.org>
2022-10-14 17:56:16 +0200

    wip: add simulated genKeyPair, getSymKey, encSym, decSym functions

--- iconet/enc_handlers.php
+++ iconet/enc_handlers.php
@@ -1,18 +1,36 @@
 <?php
-genKeyPair():keypair //['public','private']
+//test
+$key = genSymKey();
+$test = "hello";
+$enc = encSym($test, $key);
+echo $enc . "<br>";
+$dec = decSym($enc,$key);
+echo $dec;
 
-genSymKey():$symKey
+function genKeyPair(){
+    // simulated genKey
+    $pubKey='pubKey';
+    $privKey='privKey';
+    return [$pubKey,$privKey];
+} // :keypair['public','private']
 
-encSym($data,$symKey):$encrypted
+function genSymKey(){
+    //simulated genSymKey
+    $symKey='symKey';
+    return $symKey;
+} // :$symKey
 
-decSym($encrypted,$symKey):$data
+function encSym($data,$symKey){
+    //simulated encSym
+    $encrypted=$data.$symKey;
+    return $encrypted;
+} // :$encrypted
 
-genAllTokens($userLoggedIn,$symKey):tokenlist //array($address,$token)
-
-encAsym($data,$pubKey):$encrypted
-
-decAsym($encrypted,$privKey):$data
-
-openToken($token):$symKey
+function decSym($encrypted,$symKey){
+    //simulated decSym
+    $len = strlen($symKey);
+    $data=substr($encrypted,0,-$len);
+    return $data;
+} //:$data
 
 ?>
