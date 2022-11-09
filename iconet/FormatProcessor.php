133a7269b07f915da1d2d945b34def5eaf5a935e
steffen <steffen@iconet-foundation.org>
2022-11-10 00:20:00 +0100

    update FormatProcessor - use regex

--- iconet/FormatProcessor.php
+++ iconet/FormatProcessor.php
@@ -2,52 +2,48 @@
 
 namespace Iconet;
 
-use Cassandra\Collection;
 
 class FormatProcessor
 {
+
     protected string $separator_a;
     protected string $separator_b;
+    protected array $content;
+
 
     public function __construct(){
-        $this->separator_a = "['";
-        $this->separator_b = "']";
+        $this->separator_a = "\['"; // [ needs escaping in regex
+        $this->separator_b = "'\]";
     }
 
 
-    function mergeFormat(string $format, array|string $content) : string
+    /**
+     * @param string $format
+     * @param array<string> $content
+     * @return string|null
+     */
+    function mergeFormat(string $format, array $content) : string|null
     {
+        $this->content = $content;
+        $regex = "|(". $this->separator_a. ")([a-z]*)(".$this->separator_b.")|";
 
-        $separator_a = $this->separator_a;
-        $separator_b = $this->separator_b;
+        $inserted = preg_replace_callback($regex,
+            function ($matches)
+            {
+                $content = $this->content;
 
-        //find first usage of separator_a
-        $substring_start= strpos($format, $separator_a);
-
-        if(!$substring_start) return $format; //formating done if no more separator
-
-        //substring starts, after separator
-        $substring_start += strlen($separator_a);
-
-        //stringlength of substring
-        $size = strpos($format, $separator_b, $substring_start) - $substring_start;
-        //extract substring
-        $substring = substr($format, $substring_start, $size);
-        //check if substring is a field in content
-        if(!isset($content[$substring])){
-            $error =  "Error: Missing field in Content Package: ". $substring . "<br>";
-            echo $error;
-            return $error;
-        }
-
-        // separators + substring are to be replace
-        $replace = $separator_a . $substring . $separator_b;
-        // replace format placeholder with given content
-        $inserted = str_replace($replace, $content[$substring], $format);
-
-        //replaced first occurence. replace entire format recursivly.
-        $inserted = $this->mergeFormat($inserted, $content);
+                if(!isset($content[$matches[2]])){
+                    $error =  "Error: Missing field in Content Package: ". $matches[2] . "<br>";
+                     return $error;
+                }
+                return $content[$matches[2]];
+            },
+            $format);
 
         return $inserted;
     }
+
+
+
+
 }
