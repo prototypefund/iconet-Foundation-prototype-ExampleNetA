c460415f693d7336b64ae6bbea6e2be360eefeb0
steffen <steffen@iconet-foundation.org>
2022-11-10 15:17:34 +0100

    improve regex, make separators constants

--- iconet/FormatProcessor.php
+++ iconet/FormatProcessor.php
@@ -5,36 +5,29 @@ namespace Iconet;
 
 class FormatProcessor
 {
+    const separatorA = "\['"; //  [ needs escaping in regex
+    const separatorB = "'\]";
+    const regex = "|(" . self::separatorA . ")(\w+)(" . self::separatorB . ")|";
 
-    protected string $separator_a;
-    protected string $separator_b;
     protected array $content;
-
-
-    public function __construct(){
-        $this->separator_a = "\['"; // [ needs escaping in regex
-        $this->separator_b = "'\]";
-    }
-
 
     /**
      * @param string $format
      * @param array<string> $content
      * @return string|null
      */
-    function mergeFormat(string $format, array $content) : string|null
+    function mergeFormat(string $format, array $content): string|null
     {
         $this->content = $content;
-        $regex = "|(". $this->separator_a. ")([a-z]*)(".$this->separator_b.")|";
 
-        $inserted = preg_replace_callback($regex,
-            function ($matches)
-            {
+        $inserted = preg_replace_callback(
+            self::regex,
+            function($matches) {
                 $content = $this->content;
 
-                if(!isset($content[$matches[2]])){
-                    $error =  "Error: Missing field in Content Package: ". $matches[2] . "<br>";
-                     return $error;
+                if(!isset($content[$matches[2]])) {
+                    $error = "Error: Missing field in Content Package: " . $matches[2] . "<br>";
+                    return $error;
                 }
                 return $content[$matches[2]];
             },
