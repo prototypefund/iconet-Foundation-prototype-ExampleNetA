1116e96e93f7a5032e19edaa1c7c63d8fe66b8bd
Martin Miller <martin.mil@web.de>
2022-12-07 12:54:26 +0100

    Describe HTTP headers in the evil-inbox format
    Add header X-Frame-Options and explain why this does not work yet.

--- iconet/formats/evil-inbox/evil.js
+++ iconet/formats/evil-inbox/evil.js
@@ -1,3 +1,16 @@
+/*
+    This evil format opens the users entire inbox as a format template in an iframe.
+
+    Setting these HTTP headers does not help:
+
+            Header add X-Frame-Options: "DENY"
+                The srcdoc workaround bypasses this.
+
+            Header add Access-Control-Allow-Origin "..."
+                The client still can fetch and embed itself (same origin)
+ */
+
+
 window.onload = () => {
     const node = document.createElement("h2")
     node.innerText = document.querySelector("embedded-experience").content
