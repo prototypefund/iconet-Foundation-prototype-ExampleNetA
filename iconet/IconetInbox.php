b57d6a1723c945599f5120ee76b0bd6171ab8a08
Peter Brandl <peter.brandl@your.company>
2023-01-18 14:23:04 +0100

    s2s receiver save notficiation

---
+++ iconet/IconetInbox.php
@@ -0,0 +1,49 @@
+<?php
+namespace Iconet;
+
+use Iconet\Crypto;
+use Iconet\Database;
+use Iconet\User;
+
+
+class IconetInbox
+{
+    private User $user;
+
+    private Database $database;
+    private Crypto $crypto;
+
+
+    /**
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->database = new Database();
+        $this->crypto = new Crypto();
+        $this->user = $user;
+    }
+
+    public function saveNotification(object $packet): bool
+    {           
+        $id = $packet->id;
+        $actor = $packet->actor;
+        $encryptedSecret = $packet->encryptedSecret;
+        $encryptedContent = $packet->encryptedContent;
+        $encryptedFormatId = $packet->encryptedFormatId;
+        $privateKey = $this->user->privateKey;
+        $secret = $this->crypto->decAsym($encryptedSecret, $privateKey);
+
+        $content = json_decode($this->crypto->decSym($encryptedContent, $secret));
+        $formatId = json_decode($this->crypto->decSym($encryptedFormatId, $secret));
+
+        if(!$content) 
+        {
+            $content = "Decryption Error.";
+        }
+
+        $this->database->addNotification($id, $this->user->username, $actor, $secret, $content);
+        //todo check for errors
+        return true;
+    }
+}
--- iconet/PacketHandler.php
+++ iconet/PacketHandler.php
@@ -12,112 +12,29 @@ class PacketHandler
      * @param mixed $packet
      * @return PacketTypes The determined type of the packet (includes INVALID)
      */
-    public static function checkPacket(mixed $packet): PacketTypes
+    public static function checkPacket(mixed $packet): bool
     {
-        if(!isset($packet->type)) {
-            echo "Error - Must set field 'type' <br>";
-            return PacketTypes::ERROR;
+        // Check if non-optional variables are set.
+        if(isset($packet->{'@context'}) &&
+           isset($packet->{'id'}) &&
+           isset($packet->{'actor'}) && 
+           isset($packet->{'to'}) &&
+           isset($packet->{'encryptedSecret'}) &&
+           isset($packet->{'encryptedContent'}) &&
+           isset($packet->{'encryptedFormatId'})
+           ) {
+            // Check if non-optional variables are proper (can't check notification content, potentially encrypted)
+            if(Address::validate($packet->actor) && Address::validate($packet->to)) {
+                // All conditions for type send notification are met.
+                return true;
+            } else {
+                echo "Error - Faulty actor or recipient address <br>";
+                return false;
+            }
+        } else {
+            echo "Error - Missing field ('actor'/'predata'/'ecryptedSecret'/'to') <br>";
+            return false;
         }
-
-        $type = PacketTypes::tryFrom($packet->type);
-        if(!$type) {
-            return PacketTypes::ERROR;
-        }
-
-        switch($type) {
-            case PacketTypes::PUBLIC_KEY_REQUEST:
-                //check if non-optional variables are set.
-                if(isset($packet->address)) {
-                    //check if non-optional variables are proper
-                    if(Address::validate($packet->address)) {
-                        //all conditions for publicKey request are met.
-                        return PacketTypes::PUBLIC_KEY_REQUEST;
-                    } else {
-                        echo "Error - Faulty address in publicKey request <br>" . $packet->address . "<br>";
-                        return PacketTypes::ERROR;
-                    }
-                } else {
-                    echo "Error - Missing address in publicKey request <br>";
-                    return PacketTypes::ERROR;
-                }
-
-            case PacketTypes::PUBLIC_KEY_RESPONSE:
-                //check if non-optional variables are set.
-                if(isset($packet->address) && isset($packet->publicKey)) {
-                    //check if non-optional variables are proper
-                    if(Address::validate($packet->address)) {
-                        //all conditions for publicKey request are met.
-                        return PacketTypes::PUBLIC_KEY_RESPONSE;
-                    } else {
-                        echo "Error - Faulty address in publicKey response <br>" . $packet->address . "<br>";
-                        return PacketTypes::ERROR;
-                    }
-                } else {
-                    echo "Error - Missing field in publicKey response ('address'/'publicKey') <br>";
-                    return PacketTypes::ERROR;
-                }
-
-            case PacketTypes::NOTIFICATION:
-                //check if non-optional variables are set.
-                if(isset($packet->actor) && isset($packet->predata) && isset($packet->encryptedSecret) && isset($packet->to)) {
-                    //check if non-optional variables are proper (can't check notification content, potentially encrypted)
-                    if(Address::validate($packet->actor) && Address::validate($packet->to)) {
-                        //all conditions for type send notification are met.
-                        return PacketTypes::NOTIFICATION;
-                    } else {
-                        echo "Error - Faulty actor or recipient address <br>";
-                        return PacketTypes::ERROR;
-                    }
-                } else {
-                    echo "Error - Missing field in notification ('actor'/'predata'/'ecryptedSecret'/'to') <br>";
-                    return PacketTypes::ERROR;
-                }
-
-            case PacketTypes::CONTENT_REQUEST:
-                //check if non-optional variables are set.
-                if(isset($packet->id) && isset($packet->actor)) {
-                    return PacketTypes::CONTENT_REQUEST;
-                } else {
-                    echo "Error - Missing field in content request ('id'/'actor') <br>";
-                    return PacketTypes::ERROR;
-                }
-
-            case PacketTypes::CONTENT_RESPONSE:
-                //check if non-optional variables are set.
-                if(isset($packet->actor) && isset($packet->formatId) && isset($packet->content)) {
-                    if(Address::validate($packet->actor)) {
-                        //all conditions for type send interaction are met.
-                        return PacketTypes::CONTENT_RESPONSE;
-                    } else {
-                        echo "Error - Faulty actor address <br>";
-                        return PacketTypes::ERROR;
-                    }
-                } else {
-                    echo "Error - Missing field in content response ('actor'/'formatId'/'content')<br>";
-                    return PacketTypes::ERROR;
-                }
-
-            case PacketTypes::INTERACTION:
-                //check if non-optional variables are set.
-                if(isset($packet->id) && isset($packet->actor) && isset($packet->payload)) {
-                    //check if non-optional variables are proper (can't check notification content, potentially encrypted)
-                    if(Address::validate($packet->actor)) {
-                        //all conditions for type send interaction are met.
-                        return PacketTypes::INTERACTION;
-                    } else {
-                        echo "Error - Faulty actor address<br>";
-                        return PacketTypes::ERROR;
-                    }
-                } else {
-                    echo "Error - Missing field in interaction ('id'/'actor'/'payload')<br>";
-                    return PacketTypes::ERROR;
-                }
-
-            default:
-                echo "Unknown packet type: <br>" . $packet->type . "<br>";
-                return PacketTypes::ERROR;
-        }
-
     }
 
 }
