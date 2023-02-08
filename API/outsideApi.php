f3f213b2bc2b49e1c525d0b543e99f8baefa8387
steffen <steffen@iconet-foundation.org>
2023-02-08 09:50:39 +0100

    progress outsideApi

--- API/outsideApi.php
+++ API/outsideApi.php
@@ -1,39 +1,35 @@
 <?php
 
-$respone["a"] = "A";
-$respone["b"] = "B";
-echo json_encode($respone);
-exit;
+$error = "";
 
 if(!$_SERVER['REQUEST_METHOD'] == 'POST') {
-    echo http_response_code(400);
-    exit;
+    $error = "no post";
 }
-
 
 $body = file_get_contents('php://input');
 
 if($body === false) {
-    echo http_response_code(400);
-    exit;
+    $error = "no body;";
 }
 
 $packet = json_decode($body);
 if(!$packet) {
-    echo http_response_code(400);
-    exit;
+    $error = " no packet";
 }
 
 
 if(isset($packet->id, $packet->user)) {
     $post = Database::singleton()->getPost($packet->id, $packet->user);
     if($post != null) {
-        return json_encode($post);
+        echo json_encode($post);
+        exit;
     } else {
-        echo http_response_code(300); //no results
+        $error = "no results"; //no results
     }
 } else {
-    echo http_response_code(440); //bad querry
+    $erorr = "bad querry"; //bad querry
 }
+echo json_encode($error);
+exit;
 
 ?>
